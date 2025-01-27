cd6e96beafa9cbfd79c334f7885c5b100f390a35
Adam Gąsowski <gander@users.noreply.github.com>
2025-01-28 00:40:51 +0100

    💀

--- S03E03/app.js
+++ S03E03/app.js
@@ -1,3 +1,5 @@
+import fs from 'fs/promises'
+
 async function fetchData(query){
     return new Promise(async (resolve, reject) => {
         const response = await fetch('https://centrala.ag3nts.org/apidb', {
@@ -27,13 +29,19 @@ async function fetchData(query){
 
 
 
-const response = await fetch('https://centrala.ag3nts.org/report', {
-    method: 'POST',
-    body: JSON.stringify({
-        "task": "database",
-        "apikey": process.env.AZYL_API_KEY,
-        "answer": [4278, 9294],
-    })
-});
+// const response = await fetch('https://centrala.ag3nts.org/report', {
+//     method: 'POST',
+//     body: JSON.stringify({
+//         "task": "database",
+//         "apikey": process.env.AZYL_API_KEY,
+//         "answer": [4278, 9294],
+//     })
+// });
+//
+// console.log(await response.text());
 
-console.log(await response.text());
+// await fs.writeFile('connections.json', JSON.stringify(await fetchData('select * from connections')));
+// await fs.writeFile('correct_order.json', JSON.stringify(await fetchData('select * from correct_order order by weight')));
+await fs.writeFile('flag.txt', JSON.stringify(await fetchData('SELECT GROUP_CONCAT(letter ORDER BY weight SEPARATOR "") as result FROM correct_order')));
+// await fs.writeFile('datacenters.json', JSON.stringify(await fetchData('select * from datacenters')));
+// await fs.writeFile('users.json', JSON.stringify(await fetchData('select * from users')));
